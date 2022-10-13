7d3780b0843bd0910d33f7edf5c1e003bb060d8a
deesclouds <deescloud@protonmail.com>
2022-10-13 14:28:23 -0700

    update dependencies

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client({ intents: 32767 });
+
 require('dotenv').config()
 
+// this is our event
 client.on('ready', () => {
 	console.log('Bot is online!');
     client.guilds.cache.get('968649174759399484').commands.create({
@@ -32,14 +35,16 @@ client.on('interactionCreate', async interaction => {
         const washy = interaction.options.getNumber('washy');
 
         interaction.reply({
-            content: `Ok you said ${dasy} and ${washy}`,
+            content: `Ok you said ${dashy} and ${washy}`,
             ephemeral: true
         })
     }
 })
 
 client.on('messageCreate', (message) => {
-	const prefix = '-';
+	// this is our command
+    const prefix = '-';
+    // if the message content doesn't start with our prefix it returns  
 	if (!message.content.startsWith(prefix)) return;
 
 	const messageArray = message.content.split('');
@@ -67,14 +72,15 @@ client.on('messageCreate', (message) => {
 		if (!args[0]) return message.reply('Specify the member you want to kick!');
 		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.username.toLowerCase() === args.slice(0).join('') || x.user.username === args[0]);
 		if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply('Where are your permissions?');
-		if (!message.guild.me.permissions.has('BAN_MEMBERS')) return message.reply('You do no have permission to ban members from this server!');
+		if (!message.guild.me.permissions.has('BAN_MEMBERS')) return message.reply('You do not have permission to ban members from this server!');
 		if (message.member.id === member.id) return message.reply('You cannot ban yourself are you stupid?');
 
         let reason = arg.slice(1).join('') || 'No Reason'
 
 		member.ban({reason: reason});
 		message.channel.send(`${member} was just banned from the server. \nReason: ${reason}`);
+    
 
-})
+};
 
 client.login('token');
